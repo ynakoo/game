489ae7827ae3ace0ce63288754dbc43071273889
ynakoo <aryakoyna@gmail.com>
2025-03-29 19:33:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '/Users/ynako/react-app/my_project/src/index.css'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
